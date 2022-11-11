c3f22f0322aa84a38b4ba6ddef0daff9e31c7524
Djole-zr <93897601+Djole-zr@users.noreply.github.com>
2022-11-11 02:07:12 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const naruciDugme = document.querySelector('button');
+
+function proveriIme () {
+    const ime = document.querySelector("#ime").value;
+    const regEx = /.+/;
+    if (regEx.test(ime)) {
+        localStorage.setItem('ime', ime);
+        proveriPrezime();
+    } else {
+        alert('Ime nije ispravano');
+    }
+}
+
+function proveriPrezime () {
+    const prezime = document.querySelector("#prezime").value;
+    const regEx = /.+/;
+    if (regEx.test(prezime)) {
+        localStorage.setItem('prezime', prezime);
+        proveriAdresu();
+    } else {
+        alert('Prezime nije ispravano');
+    }
+}
+
+function proveriAdresu () {
+    const adresa = document.querySelector("#adresa").value;
+    const regEx = /.+/;
+    if (regEx.test(adresa)) {
+        localStorage.setItem('adresa', adresa);
+        proveriJMBG();
+    } else {
+        alert('Adresa nije ispravana');
+    }
+}
+
+function proveriJMBG () {
+    const jmbg = document.querySelector("#jmbg").value;
+    const regEx = /^\d{11}10$/
+    if (regEx.test(jmbg)) {
+        localStorage.setItem('jmbg', jmbg)
+        sacuvajKategoriju()
+    } else {
+        alert('JMBG nije ispravan');
+    }
+}
+
+function sacuvajKategoriju () {
+    const kategorije = document.getElementsByName('kategorija');
+            for(let kategorija of kategorije) {
+                if(kategorija.checked) {
+                    localStorage.setItem('kategorija', kategorija.value);
+                    sacuvajRate();
+                }
+            }
+}
+
+function sacuvajRate () {
+    const rate = document.querySelector('#rate').value;
+    localStorage.setItem('rate', rate);
+}
+
+
+
+naruciDugme.addEventListener('click', function (event) {
+    event.preventDefault();
+    proveriIme();
+    window.location.replace('potvrda.html');
+})
+
---
+++ potvrda.js
@@ -0,0 +1,26 @@
+function popuniTabelu () {
+    document.querySelector('#potvrda-ime').innerText = localStorage.getItem('ime');
+    document.querySelector('#potvrda-prezime').innerText = localStorage.getItem('prezime');
+    document.querySelector('#potvrda-adresa').innerText = localStorage.getItem('adresa');
+    document.querySelector('#potvrda-jmbg').innerText = localStorage.getItem('jmbg');
+    document.querySelector('#potvrda-kategorija').innerText = localStorage.getItem('kategorija');
+    document.querySelector('#ukupna-cena').innerText = ukupnaCena() + " din";    
+    document.querySelector('#potvrda-rate').innerText = localStorage.getItem('rate');
+    document.querySelector('#iznos-rate').innerText = Math.round(ukupnaCena() / localStorage.getItem('rate'));
+}
+
+function ukupnaCena () {
+    const kategorija = localStorage.getItem('kategorija');
+    if(kategorija === 'a') {
+        return 40000
+    } else if (kategorija === 'b') {
+        return 80000
+    } else if (kategorija === 'c') {
+        return 100000
+    }
+}
+
+
+window.onload = function() {
+    popuniTabelu();
+  };
